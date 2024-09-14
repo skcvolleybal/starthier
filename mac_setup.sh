2d697e395380b997e4ac49a3a58ce9d62f25eebd
Koen Schellens <k.schellens@gmail.com>
2024-09-14 15:52:54 +0200

    More xampp setup

--- mac_setup.sh
+++ mac_setup.sh
@@ -12,22 +12,81 @@ define_variables() {
 
     WP_DIR=$TARGET_DIR
 
+    DMG_URL="https://sourceforge.net/projects/xampp/files/XAMPP%20Mac%20OS%20X/8.2.4/xampp-osx-8.2.4-0-installer.dmg"
+    # DMG_URL="http://localhost/xampp-osx-8.2.4-0-installer.dmg" # For testing purposes
+    DMG_NAME="xampp-osx-8.2.4-0-installer.dmg"
+    INSTALLER_NAME="xampp-osx-8.2.4-0-installer.app"
+    MOUNT_POINT="/Volumes/XAMPP"
+
+}
+
+install_xampp () {
+    # Download the .dmg file
+    echo "Downloading $DMG_NAME..."
+    curl -L -o "$DMG_NAME" "$DMG_URL"
+
+    # Mount the .dmg file
+    echo "Mounting $DMG_NAME..."
+    hdiutil attach "$DMG_NAME" -mountpoint "$MOUNT_POINT" -nobrowse
+
+    # Check if the mount was successful
+    if [ $? -ne 0 ]; then
+    echo "Failed to mount $DMG_NAME."
+    exit 1
+    fi
+
+
+    if [ -z "$INSTALLER_NAME" ]; then
+    echo "No installer found in the mounted volume."
+    hdiutil detach "$MOUNT_POINT"
+    exit 1
+    fi
+
+    # Start the installer
+    echo "Starting installer: ${MOUNT_POINT}/${INSTALLER_NAME}..."
+    open "${MOUNT_POINT}/${INSTALLER_NAME}"
+
+    # Wait for the installation to complete
+    echo "Waiting for installation to complete..."
+        # Display a message
+    echo "Once you have installed XAMPP and started Apache and MySQL, press Enter to continue..."
+
+    # Wait for user input
+    read
+
+    # Unmount the .dmg
+    # echo "Unmounting $DMG_NAME..."
+    # hdiutil detach "$MOUNT_POINT"
+
+    # Clean up
+    rm "$DMG_NAME"
+
+    echo "Installation completed."
+
 }
 
 
-setup_xampp_and_brew() {
+
+
+setup_brew() {
     # Install Homebrew
     /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 
     echo "Installed Brew"
 
-    # Disable gatekeeper so we can install XAMPP using brew 
-    sudo spctl --master-disable
+    # # Disable gatekeeper so we can install XAMPP using brew 
+    # sudo spctl --master-disable
 
-    echo "Disabled Gatekeeper"
+    # echo "Disabled Gatekeeper"
 
-    # Install XAMPP
-    brew install --cask xampp
+    # # Install XAMPP
+    # brew install --cask xampp
+
+    # Install Node 18
+    brew install node@18
+
+    # Install Composer globally
+    brew install composer
 
     # Used to interface with WP from command line
     echo "Installing WP CLI"
@@ -143,13 +202,36 @@ install_wordpress() {
         exit 1
     fi
     echo "WordPress is now fully set up at $WP_URL."
+    echo "This is a CLEAN installation without themes or anything. Use admin panel. " 
+}
+
+install_teamportal () {
+    cd $TARGET_DIR
+    git clone https://github.com/skcvolleybal/team-portal
+    cd "${TARGET_DIR}/team-portal/"
+    npm i 
+    cd php
+    composer install 
+    cp .env.example .env
+
 
 }
 
 
+start_teamportal () {
+    cd "${TARGET_DIR}/team-portal/"
+    npm run ng serve
+
+}
+
 define_variables
-# setup_xampp_and_brew
+install_xampp
+# setup_brew
 # download_wordpress_and_plugins
-create_wordpress_database
-setup_wp_config_file
-install_wordpress
+# create_wordpress_database
+# setup_wp_config_file
+# install_wordpress
+# install_teamportal
+# start_teamportal
+
+
