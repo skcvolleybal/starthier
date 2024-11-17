ee9a102bc0ec35f5bc71eac2f8bd183b98a5129c
KM Schellens <k.schellens@gmail.com>
2024-11-17 19:37:53 +0100

    working wordpress install

--- scripts/wordpress_startup.sh
+++ scripts/wordpress_startup.sh
@@ -1,16 +1,9 @@
 #!/bin/bash
 # (wordpress_)startup.sh
 
-# Function to check if MySQL is ready
-check_mysql_ready() {
-    echo "Checking if MySQL is ready..."
-    /wait-for-it.sh mysql_db:3306
-}
 
-# Check MySQL readiness
-check_mysql_ready
-
-echo "MySQL is ready"
+echo "Checking if MySQL is ready..."
+/wait-for-it.sh -t 0 mysql_db:3306
 
 # Ensure WP-CLI is installed
 install_wp_cli() {
@@ -29,10 +22,13 @@ install_wp_cli() {
 # Install WP-CLI if needed
 install_wp_cli
 
+wp core install --allow-root --url="localhost:8080" --title="SKC WordPress Dev Site" --admin_user="webcie" --admin_email="webcie@skcvolleybal.nl" --admin_password="webcie"
+
 # Run WP-CLI commands here
 # Example: Install Pods plugin
 echo "Installing Pods plugin..."
 wp plugin install pods --allow-root --path=/var/www/html
+wp plugin install members --allow-root --path=/var/www/html
 
 # Start Apache in the foreground
 apache2-foreground
