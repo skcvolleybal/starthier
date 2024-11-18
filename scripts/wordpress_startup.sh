cc996356e6acbd78421f94f354baaa941d673e3a
KM Schellens <k.schellens@gmail.com>
2024-11-18 19:51:01 +0100

    make sure line endings are enforced unix style

--- scripts/wordpress_startup.sh
+++ scripts/wordpress_startup.sh
@@ -1,19 +1,16 @@
 #!/bin/bash
 # (wordpress_)startup.sh
-
-
 echo "Checking if MySQL is ready..."
 /wait-for-it.sh -t 0 mysql_db:3306
-
 wp core install --url='http://localhost:8080' --title='SKC Volleybal Dev Site' --admin_user='webcie' --admin_password='webcie' --admin_email='webcie@skcvolleybal.nl' --path='/var/www/html' --skip-email
 wp option update blogdescription 'Development site for SKC Volleybal' 
 wp user update webcie --first_name='Webcie' --last-name='SKC' --path='/var/www/html'
+
 
 # Example: Install Pods plugin
 echo "Installing Pods plugin..."
 wp plugin install pods --allow-root --path=/var/www/html
 wp plugin install members --allow-root --path=/var/www/html
-
 # Activate them bad boys
 wp plugin activate pods --allow-root --path=/var/www/html
 wp plugin activate members --allow-root --path=/var/www/html
@@ -34,7 +31,5 @@ wp role list --allow-root --path=/var/www/html | grep -q 'abonnee' || wp role cr
 wp role list --allow-root --path=/var/www/html | grep -q 'tc' || wp role create tc 'TC' --allow-root --path=/var/www/html &&
 wp role list --allow-root --path=/var/www/html | grep -q 'teamcoordinator' || wp role create teamcoordinator 'Teamcoordinator' --allow-root --path=/var/www/html &&
 wp role list --allow-root --path=/var/www/html | grep -q 'webcie' || wp role create webcie 'WebCie' --allow-root --path=/var/www/html &&
-
-
 wp pods pod add --type=post_type --name=team --allow-root --path=/var/www/html
 
