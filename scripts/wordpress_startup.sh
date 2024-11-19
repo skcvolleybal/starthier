217834468029040fcc4e690e7db47234e64872ef
Koen Schellens <k.schellens@gmail.com>
2024-11-19 13:17:08 +0100

    Container stays up after running commands

--- scripts/wordpress_startup.sh
+++ scripts/wordpress_startup.sh
@@ -18,19 +18,23 @@ wp plugin activate members --allow-root --path=/var/www/html
 
 
 # Create custom roles only if they don't already exist
-wp role list --allow-root --path=/var/www/html | grep -q 'beheerder' || wp role create beheerder 'Beheerder' --allow-root --path=/var/www/html &&
-wp role list --allow-root --path=/var/www/html | grep -q 'barcie' || wp role create barcie 'Barcie' --allow-root --path=/var/www/html &&
-wp role list --allow-root --path=/var/www/html | grep -q 'bestuur' || wp role create bestuur 'Bestuur' --allow-root --path=/var/www/html &&
-wp role list --allow-root --path=/var/www/html | grep -q 'commissieco' || wp role create commissieco 'CommissieCo' --allow-root --path=/var/www/html &&
-wp role list --allow-root --path=/var/www/html | grep -q 'customer' || wp role create customer 'Customer' --allow-root --path=/var/www/html &&
-wp role list --allow-root --path=/var/www/html | grep -q 'lustrumcie' || wp role create lustrumcie 'LustrumCie' --allow-root --path=/var/www/html &&
-wp role list --allow-root --path=/var/www/html | grep -q 'paparazcie' || wp role create paparazcie 'PaparazCie' --allow-root --path=/var/www/html &&
-wp role list --allow-root --path=/var/www/html | grep -q 'scheidsco' || wp role create scheidsco 'ScheidsCo' --allow-root --path=/var/www/html &&
-wp role list --allow-root --path=/var/www/html | grep -q 'vendor' || wp role create vendor 'Vendor' --allow-root --path=/var/www/html &&
-wp role list --allow-root --path=/var/www/html | grep -q 'shop' || wp role create shop 'Shop' --allow-root --path=/var/www/html &&
-wp role list --allow-root --path=/var/www/html | grep -q 'abonnee' || wp role create abonnee 'Abonnee' --allow-root --path=/var/www/html &&
-wp role list --allow-root --path=/var/www/html | grep -q 'tc' || wp role create tc 'TC' --allow-root --path=/var/www/html &&
-wp role list --allow-root --path=/var/www/html | grep -q 'teamcoordinator' || wp role create teamcoordinator 'Teamcoordinator' --allow-root --path=/var/www/html &&
-wp role list --allow-root --path=/var/www/html | grep -q 'webcie' || wp role create webcie 'WebCie' --allow-root --path=/var/www/html &&
+wp role list --allow-root --path=/var/www/html | grep -q 'beheerder' || wp role create beheerder 'Beheerder' --allow-root --path=/var/www/html 
+wp role list --allow-root --path=/var/www/html | grep -q 'barcie' || wp role create barcie 'Barcie' --allow-root --path=/var/www/html 
+wp role list --allow-root --path=/var/www/html | grep -q 'bestuur' || wp role create bestuur 'Bestuur' --allow-root --path=/var/www/html 
+wp role list --allow-root --path=/var/www/html | grep -q 'commissieco' || wp role create commissieco 'CommissieCo' --allow-root --path=/var/www/html 
+wp role list --allow-root --path=/var/www/html | grep -q 'customer' || wp role create customer 'Customer' --allow-root --path=/var/www/html 
+wp role list --allow-root --path=/var/www/html | grep -q 'lustrumcie' || wp role create lustrumcie 'LustrumCie' --allow-root --path=/var/www/html 
+wp role list --allow-root --path=/var/www/html | grep -q 'paparazcie' || wp role create paparazcie 'PaparazCie' --allow-root --path=/var/www/html 
+wp role list --allow-root --path=/var/www/html | grep -q 'scheidsco' || wp role create scheidsco 'ScheidsCo' --allow-root --path=/var/www/html 
+wp role list --allow-root --path=/var/www/html | grep -q 'vendor' || wp role create vendor 'Vendor' --allow-root --path=/var/www/html 
+wp role list --allow-root --path=/var/www/html | grep -q 'shop' || wp role create shop 'Shop' --allow-root --path=/var/www/html 
+wp role list --allow-root --path=/var/www/html | grep -q 'abonnee' || wp role create abonnee 'Abonnee' --allow-root --path=/var/www/html 
+wp role list --allow-root --path=/var/www/html | grep -q 'tc' || wp role create tc 'TC' --allow-root --path=/var/www/html 
+wp role list --allow-root --path=/var/www/html | grep -q 'teamcoordinator' || wp role create teamcoordinator 'Teamcoordinator' --allow-root --path=/var/www/html 
+wp role list --allow-root --path=/var/www/html | grep -q 'webcie' || wp role create webcie 'WebCie' --allow-root --path=/var/www/html 
 wp pods pod add --type=post_type --name=team --allow-root --path=/var/www/html
 
+echo "✅  Done running all preprogrammed commands, WP CLI container remains available"
+
+# Infinite loop to keep the container alive
+while true; do sleep 3600; done
