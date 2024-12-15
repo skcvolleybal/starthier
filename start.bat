65388289adae759fa184f37d276e9afbe363c29b
Koen Schellens <k.schellens@gmail.com>
2024-12-16 00:38:49 +0100

    windows update

--- start.bat
+++ start.bat
@@ -5,16 +5,13 @@ IF EXIST "team-portal" (
     echo Deleting existing team-portal directory...
     rmdir /S /Q "team-portal"
 )
-echo Cloning Team-Portal repository...
-git clone https://github.com/skcvolleybal/team-portal.git team-portal
 
 REM Delete and clone TC-App repository
 IF EXIST "tc-app" (
     echo Deleting existing tc-app directory...
     rmdir /S /Q "tc-app"
 )
-echo Cloning TC-App repository...
-git clone https://github.com/skcvolleybal/tc-app.git tc-app
+git submodule update --init --recursive
 
 REM Build and start Docker Compose services
 echo Building and starting Docker Compose services...
