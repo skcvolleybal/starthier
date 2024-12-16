57307eda21ad764ff35399001522b0fce56bb673
Koen Schellens <k.schellens@gmail.com>
2024-12-16 12:36:11 +0100

    without deleting repos

--- start.sh
+++ start.sh
@@ -4,23 +4,7 @@ command -v docker &>/dev/null || { echo "❌ Docker is not installed. Please ins
 pgrep -x "docker" &>/dev/null || { echo "❌ Docker is installed but not running. Please start Docker and try again."; exit 1; }
 echo "✅ Docker is running. Continuing..."
 
-
-# Delete and clone Team-Portal repository
-if [ -d "team-portal" ]; then
-    echo "Deleting existing team-portal directory..."
-    rm -rf "team-portal"
-    echo "Existing Team-Portal removed"
-fi
-
-# Delete and clone TC-App repository
-if [ -d "tc-app" ]; then
-    echo "Deleting existing tc-app directory..."
-    rm -rf "tc-app"
-    echo "Existing Team-Portal removed"
-fi
-
 git submodule update --init --recursive  # This clones the submodule if not already done
-
 
 # Build and start Docker Compose services
 echo "Building and starting Docker Compose services..."
