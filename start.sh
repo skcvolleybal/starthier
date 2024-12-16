60255d3c9afb3b3d26acecd9842626f379813101
Koen Schellens <k.schellens@gmail.com>
2024-12-16 12:03:54 +0100

    back to gitmodules

--- start.sh
+++ start.sh
@@ -1,17 +1,27 @@
 #!/bin/bash
 
-# Check if Docker is installed and running
 command -v docker &>/dev/null || { echo "❌ Docker is not installed. Please install Docker and try again."; exit 1; }
 pgrep -x "docker" &>/dev/null || { echo "❌ Docker is installed but not running. Please start Docker and try again."; exit 1; }
 echo "✅ Docker is running. Continuing..."
 
-# Exit immediately if a command exits with a non-zero status
-set -e
 
-git subtree add --prefix="team-portal" "https://github.com/skcvolleybal/team-portal.git" "master" 
-git subtree add --prefix="tc-app" "https://github.com/skcvolleybal/tc-app.git" "master" 
+# Delete and clone Team-Portal repository
+if [ -d "team-portal" ]; then
+    echo "Deleting existing team-portal directory..."
+    rm -rf "team-portal"
+    echo "Existing Team-Portal removed"
+fi
 
-echo "Subtrees added successfully."
+# Delete and clone TC-App repository
+if [ -d "tc-app" ]; then
+    echo "Deleting existing tc-app directory..."
+    rm -rf "tc-app"
+    echo "Existing Team-Portal removed"
+fi
+
+git submodule update --init --recursive
+
+
 # Build and start Docker Compose services
 echo "Building and starting Docker Compose services..."
 docker compose up
