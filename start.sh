558313f3a54c0cde1c75ce5a3f89c135bfc6ade5
Koen Schellens <k.schellens@gmail.com>
2024-12-24 13:09:23 +0100

    remove unnecessary check

--- start.sh
+++ start.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
 
 command -v docker &>/dev/null || { echo "❌ Docker is not installed. Please install Docker and try again."; exit 1; }
-pgrep -x "docker" &>/dev/null || { echo "❌ Docker is installed but not running. Please start Docker and try again."; exit 1; }
-echo "✅ Docker is running. Continuing..."
 
 git submodule update --init --recursive  # This clones the submodule if not already done
 
